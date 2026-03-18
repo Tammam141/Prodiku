9964478a76b06f4b9cd21a607518cbba5b776654
Tammam141 <tammamtanjung1405@gmail.com>
2026-03-19 02:26:33 +0700

    Persiapan untuk hosting vercel

--- config.py
+++ config.py
@@ -1,14 +1,13 @@
 import os
 
 class Config:
-    # Vercel Neon biasanya memberikan variabel DATABASE_URL
-    # Kita buat fleksibel agar bisa jalan di lokal maupun online
+    # Vercel Neon secara otomatis mengisi DATABASE_URL
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'postgresql://postgres:admin@localhost:5432/prodiku'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci-rahasia-anda'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci-rahasia-prodiku'
     
-    # Login Admin Dashboard
+    # Login Admin
     ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME') or 'postgres'
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD') or 'admin'
