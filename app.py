647ef32b84c35be25c36b0ad349e5957ab70dd04
Tammam141 <tammamtanjung1405@gmail.com>
2026-03-19 22:34:41 +0700

    10

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
